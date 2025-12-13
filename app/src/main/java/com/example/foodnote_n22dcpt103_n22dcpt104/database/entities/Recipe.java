79bda9ec6f6c0ac8c2a52359100c79e76dafc7c4
HaiTrieu186 <pnhtrieu186@gmail.com>
2025-12-13 14:57:05 +0700

    0. Khoi tao du an - tao entity dau tien: recipe

---
+++ app/src/main/java/com/example/foodnote_n22dcpt103_n22dcpt104/database/entities/Recipe.java
@@ -0,0 +1,115 @@
+package com.example.foodnote_n22dcpt103_n22dcpt104.database.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "recipe")
+public class Recipe {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private String description;
+    private String instruction;
+    public String img;
+    public String cuisine;
+    public String category;
+    @ColumnInfo(name = "ready_in_minutes")
+    public int readyInMinutes;
+    public int servings;
+    @ColumnInfo(name = "is_favorite")
+    public boolean isFavorite;
+
+    public Recipe(String name, String description, String instruction, String img, String cuisine, String category, int readyInMinutes, int servings, boolean isFavorite) {
+        this.name = name;
+        this.description = description;
+        this.instruction = instruction;
+        this.img = img;
+        this.cuisine = cuisine;
+        this.category = category;
+        this.readyInMinutes = readyInMinutes;
+        this.servings = servings;
+        this.isFavorite = isFavorite;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public int getReadyInMinutes() {
+        return readyInMinutes;
+    }
+
+    public void setReadyInMinutes(int readyInMinutes) {
+        this.readyInMinutes = readyInMinutes;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+};
---
+++ app/src/main/java/com/example/foodnote_n22dcpt103_n22dcpt104/database/entities/Shopping_list_item.java
@@ -0,0 +1,4 @@
+package com.example.foodnote_n22dcpt103_n22dcpt104.database.entities;
+
+public class Shopping_list_item {
+}
